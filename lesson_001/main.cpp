101f34ce9e5c7a4273c4312e6a64972392ef09df
Emilian Tabără <emilian.tabara@gmail.com>
2022-10-10 19:44:26 +0300

    Added first lesson

---
+++ lesson_001/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Eu sunt Gigel! ;0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+    cout << "Imi place pizza! Very good. :0" << endl;
+
+    return 0;
+}
