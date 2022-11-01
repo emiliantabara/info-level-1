92fd463114735012b7637f124d637532805dccb4
Emilian Tabără <emilian.tabara@gmail.com>
2022-11-01 17:59:53 +0200

    Added exercitiu

---
+++ exercitiu - lesson 7/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,e;
+    cin >> a >> b >>c;
+    if (c > 0) {
+        e = a+b;
+    }
+    else {
+        e = a-b;
+    }
+    cout << e;
+    return 0;
+}
