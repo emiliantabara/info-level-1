cf81e7066928981b99add66577eda520f77c852d
Emilian Tabără <emilian.tabara@gmail.com>
2022-12-11 11:35:54 +0200

    Added tema_001

--- lesson_012/tema_001/main.cpp
+++ lesson_012/tema_001/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 using namespace std;
+
 /*
 1. Se citește un numar natural n. Sa se afiseze lista de divizori ai lui n, pe acelasi
 rand, cu un spatiu intre valorile afisate (inclusiv 1 si el insusi vor fi luati în calcul).
@@ -8,11 +10,11 @@ Exemplu: pt n = 12, se va afisa 1 2 3 4 6 12
 */
 
 int main() {
-    int n,d;
+    int n, d;
     cin >> n;
     d = 1;
     while (d <= n) {
-        if (n%d == 0) {
+        if (n % d == 0) {
             cout << d << " ";
         }
         d++;
