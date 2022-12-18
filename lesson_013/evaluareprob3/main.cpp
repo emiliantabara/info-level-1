166279bea3667394b159f952bfbfd5b3c71c8bc4
Emilian Tabără <emilian.tabara@gmail.com>
2022-12-18 12:43:04 +0200

    evaluare 4

--- lesson_013/evaluareprob3/main.cpp
+++ lesson_013/evaluareprob3/main.cpp
@@ -2,20 +2,28 @@
 
 using namespace std;
 
+// https://www.pbinfo.ro/probleme/65/produscifreimpare
+
 int main() {
     int n, u, prod;
     cin >> n;
-    if (n%10 != 2) {
-        return -1;
-    }
+
+    bool toate_pare = true;
+
     prod = 1;
     while (n != 0) {
         u = n % 10;
         if (u % 2 == 1) {
             prod = prod * u;
+            toate_pare = false;
         }
         n = n / 10;
     }
-    cout << prod;
+    if (toate_pare) {
+        cout << "-1";
+    }
+    else {
+        cout << prod;
+    }
     return 0;
 }
