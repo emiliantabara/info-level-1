392a8b27758554ca8d968d88d4fee9c32556673d
Emilian Tabără <emilian.tabara@gmail.com>
2022-12-18 11:17:40 +0200

    tema 4

---
+++ lesson_014/tema_004/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+2. Ana adora numerele care contin 11. Pentru un numar natural de maxim 2.000.000.000, sa se
+spuna cati de 11 apar.
+Exemplu:
+34111511
+se va afisa: 3 (pentru ca 11 apare astfel: 34111511, 34111511, 34111511 )
+111111
+se va afisa: 5 (pentru ca 11 apare astfel: 111111, 111111, 111111, 111111, 111111)
+*/
+
+int main() {
+    int n, cifre_11, nr_11;
+    cin >> n;
+    if (n > 2000000000)
+    {
+        cout << "Nr introdus este invalid";
+        return -1;
+    }
+
+    nr_11 = 0;
+    while (n != 0) {
+        cifre_11  = n % 100;
+        if (cifre_11 == 11) {
+                nr_11++;
+        }
+        n = n/10;
+    }
+    cout << "Nr de aparitii pentru `11`: " << nr_11;
+    return 0;
+}
