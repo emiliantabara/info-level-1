bb16a35a480020b6c446efbf7f5047488bc6a2f5
Emilian Tabără <emilian.tabara@gmail.com>
2022-12-18 12:33:24 +0200

    evaluare 2

--- lesson_013/evaluareprob2/main.cpp
+++ lesson_013/evaluareprob2/main.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
+// https://www.pbinfo.ro/probleme/452/cifimp
+
 int main() {
     int n, u, nri;
     cin >> n;
     if (n > 1000 || n < 100) {
         cout << "Numarul trebuie sa fie de 3 cifre!!";
         return -1;
-    } else {
-        while (n != 0) {
-            u = n % 10;
-            if (u % 2 == 1) {
-                nri++;
-            }
-            n = n / 10;
-        }
-        cout << nri;
-        return 0;
     }
+
+    while (n != 0) {
+        u = n % 10;
+        if (u % 2 == 1) {
+            nri++;
+        }
+        n = n / 10;
+    }
+    cout << nri;
+    return 0;
 }
