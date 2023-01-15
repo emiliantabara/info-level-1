f977482775badfec6d07876771740a8e7518fe1b
Emilian Tabără <emilian.tabara@gmail.com>
2023-01-15 19:18:01 +0200

    tema pt weekend

--- lesson_016/tema_001/main.cpp
+++ lesson_016/tema_001/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+
 using namespace std;
+
+/*
+1. Se citesc doua caractere de la tastatura. Ambele caractere citite trebuie sa fie cifre,
+daca nu sunt cifre, se va afisa direct mesajul “Trebuie introduse doua cifre” si programul
+nu va mai face alt pas. Sa se formeze cel mai mare numar care se poate obtine prin
+combinarea celor doua caractere, sa se salveze intr-o variabila de tip intreg si sa se
+afiseze.
+Exemplu: pt caracterele citite 8 9, se va afisa 98.
+ */
+
 int main() {
-    int a,b,nr;
+    ushort a, b;
+    int nr;
     cin >> a >> b;
-    if (a >= 10 && b >= 10) {
+    if (a >= 10 || b >= 10) {
         cout << "a si b trebuie sa fie cifre!!";
+        return -1;
     }
     if (b > a) {
-        nr = b*10+a;
-    }
-    else {
-        nr = a*10+b;
+        nr = b * 10 + a;
+    } else {
+        nr = a * 10 + b;
     }
     cout << nr;
     return 0;
