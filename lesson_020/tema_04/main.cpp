52d4c1be9cb5b6eafd5237b9661ab8226b20602b
Emilian Tabără <emilian.tabara@gmail.com>
2023-03-05 12:44:36 +0200

    tema 4

---
+++ lesson_020/tema_04/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+/*
+3. Se citesc un numar natural n si o secventa de n numere naturale din fisierul zero_secv.in. Sa se afiseze
+in fisierul zero_secv.out cate elemente din secventa sunt egale cu zero si cate diferite de zero. (Proiectulse va numi zero_secv)
+*/
+
+int main() {
+    ifstream fin("../maxim_secv.in");
+    ofstream fout("../maxim_secv.out");
+    int n, i, nr_egal0, nr_diferit0,val;
+    fin >> n;
+    nr_diferit0 = 0;
+    nr_egal0 = 0;
+    for (i = 0; i < n; i++) {
+        fin >> val;
+        if (val == 0) {
+            nr_egal0++;
+        }
+        else {
+            nr_diferit0++;
+        }
+    }
+    fout << nr_egal0 << " " << nr_diferit0;
+    return 0;
+}
