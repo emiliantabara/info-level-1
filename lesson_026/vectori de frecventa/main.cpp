4cf8cff1662165d7acf53d3b3d8dfe527e963abc
Emilian Tabără <emilian.tabara@gmail.com>
2023-04-25 17:15:25 +0300

    introducerea in vectori de frecventa

--- lesson_026/vectori de frecventa/main.cpp
+++ lesson_026/vectori de frecventa/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int f[12]; /// declararea inainte de main --> initializare cu 0
 
 int main() {
-    int n, vot;
+    int n, vot, vmax;
     cin >> n; /// reprezinta nr de voturi
     for (int i = 0; i < n; i++) {
         cin >> vot; /// citesc votul curent
@@ -12,6 +12,18 @@ int main() {
     }
     for (int i = 0; i <= 11; i++) { /// parcurg jocurile posibile / elemente din vectorul de frecventa
         cout << i << " " << f[i] << endl;
+    }
+    vmax = 0;
+    for (int i = 0; i <= 11; i++) {
+        if (f[i] > vmax) {
+            vmax = f[i];
+        }
+    }
+    cout << vmax << endl;
+    for (int i = 0; i <= 11; i++) {
+        if (f[i] == vmax) {
+            cout << i << " ";
+        }
     }
     return 0;
 }
